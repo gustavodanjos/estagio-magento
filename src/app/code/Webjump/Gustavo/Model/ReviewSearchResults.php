d703bfa4bc49287e3762b344d9f2f5c6891bc199
gustavodanjos <gustavoanjos.aluno@unipampa.edu.br>
2026-09-17 15:48:07 -0300

    feat: add review search results implementation

---
+++ src/app/code/Webjump/Gustavo/Model/ReviewSearchResults.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Webjump\Gustavo\Model;
+
+use Magento\Framework\Api\SearchResults;
+use Webjump\Gustavo\Api\Data\ReviewSearchResultsInterface;
+
+class ReviewSearchResults extends SearchResults implements ReviewSearchResultsInterface
+{
+}
